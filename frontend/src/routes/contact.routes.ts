6ceb47a902e02ad2dae750885f6057e42d596975
dev-master21 <feelmihail@gmail.com>
2025-10-24 18:50:26 +0600

    Delete frontend directory

--- frontend/src/routes/contact.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import contactController from '../controllers/contact.controller';
-
-const router = Router();
-
-router.post('/', contactController.submitContact);
-router.post('/join-club', contactController.joinClub);
-
-export default router;
