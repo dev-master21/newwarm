ad938efa838e00af85e809c70a66ba5065f845d6
dev-master21 <feelmihail@gmail.com>
2025-10-24 18:50:35 +0600

    Delete backend directory

--- backend/src/routes/maps.routes.ts
+++
@@ -1,9 +0,0 @@
-// backend/src/routes/maps.routes.ts
-import { Router } from 'express';
-import mapsController from '../controllers/maps.controller';
-
-const router = Router();
-
-router.post('/expand-url', mapsController.expandUrl);
-
-export default router;
